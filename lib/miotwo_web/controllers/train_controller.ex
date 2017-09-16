76cf438a553259e087b86cf03cd4e96647a5551d
Jake Swanson <jakswa@gmail.com>
2017-09-16 19:50:41 -0400

    sync more closely with marta http response
    Right now we're just trying to proxy a raw response, without any
    modifications. In the future, we'll decide at which point schedule data
    gets inserted.

--- lib/miotwo_web/controllers/train_controller.ex
+++ lib/miotwo_web/controllers/train_controller.ex
@@ -2,10 +2,16 @@ defmodule MiotwoWeb.TrainController do
   use MiotwoWeb, :controller
   alias Miotwo.TrainCache
 
+  # Proxy the raw response from MARTA,
+  # including content-type, status, and body.
   def index(conn, %{}) do
     resp = TrainCache.train_json
+    content_type = Enum.find(resp.headers, fn(i) -> 
+      elem(i, 0) == "Content-Type"
+    end) |> elem(1)
+
     conn
-    |> put_resp_content_type("application/json")
+    |> put_resp_content_type(content_type)
     |> send_resp(resp.status_code, resp.body)
   end
 end
--- lib/miotwo_web/views/train_view.ex
+++
@@ -1,8 +0,0 @@
-defmodule MiotwoWeb.TrainView do
-  use MiotwoWeb, :view
-  alias Miotwo.TrainCache
-
-  def render("index.json", %{}) do
-    # TrainCache.train_json.body
-  end
-end
